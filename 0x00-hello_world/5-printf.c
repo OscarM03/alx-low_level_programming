673227c2a9596a42e6e8701e4ecdb28f666571d4
OscarM03 <oscarmutwiri03@gmail.com>
2023-07-13 16:23:35 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Shows how printf works
+ * Return: 0 (it should return zero)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
