5c170de1c6a2427f5bef0499ca164a711bc3801a
OscarM03 <oscarmutwiri03@gmail.com>
2023-07-15 00:47:46 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in reverse
+ * Return: 0 (must)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
