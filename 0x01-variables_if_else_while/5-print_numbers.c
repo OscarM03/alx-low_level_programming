896a1a04739793fd3188751da4d662f4876008a2
OscarM03 <oscarmutwiri03@gmail.com>
2023-07-15 00:11:05 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers below ten
+ * Return: 0 (must)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
