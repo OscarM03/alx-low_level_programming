deba0c3207b25b7896b7ef31127dc4bdda1bcd71
OscarM03 <oscarmutwiri03@gmail.com>
2023-07-19 06:15:55 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+
+	char h1;
+	char h2;
+	char m1;
+	char m2;
+
+	for (h1 = '0'; h1 <= '2'; h1++)
+	{
+	for (h2 = '0'; h2 <= '9'; h2++)
+	{
+	for (m1 = '0'; m1 <= '5'; m1++)
+	{
+	for (m2 = '0'; m2 <= '9'; m2++)
+	{
+		_putchar(h1);
+		_putchar(h2);
+		_putchar(':');
+		_putchar(m1);
+		_putchar(m2);
+		_putchar('\n');
+
+		if (h1 == '2' && h2 == '3' && m1 == '5' && m2 == '9')
+		{
+			exit(0);
+}
+}
+}
+}
+}
+}
