0903c58669911265de7d653e4b18eda5dd6f3cdf
OscarM03 <oscarmutwiri03@gmail.com>
2023-07-20 15:35:16 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,24 +5,33 @@
   */
 int main(void)
 {
-	int a;
+	int i;
 
-	for (a = 1; a <= 100; a++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		} else if (a % 3 == 0)
-		{
-			printf("Fizz ");
-		} else if (a % 5 == 0)
-		{
-			printf("Buzz ");
-		} else
-		{
-			printf("%d ", a);
-		}
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+	printf(" Fizz");
+	}
+	else if (i % 5 == 0 && i % 3 != 0)
+	{
+	printf(" Buzz");
+	}
+	else if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf(" FizzBuzz");
+	}
+	else if (i == 1)
+	{
+	printf("%d", i);
+	}
+	else
+	{
+	printf(" %d", i);
+	}
 	}
 	printf("\n");
+
 	return (0);
 }
+
