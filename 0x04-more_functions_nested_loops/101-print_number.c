3ac37f195ffe79c8329bf4d1b0d57456072332d1
OscarM03 <oscarmutwiri03@gmail.com>
2023-07-21 13:45:45 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,16 @@
   */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		i = -i;
 	}
-	if (n >= 10)
+	if (i / 10)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-	_putchar('0' + n % 10);
+	_putchar('0' + i % 10);
 }
