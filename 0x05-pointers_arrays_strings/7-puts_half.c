87a64a942aeedf2efac0c6a91a86cc8b81c60ea3
OscarM03 <oscarmutwiri03@gmail.com>
2023-07-25 03:13:36 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* puts_half - print the last half of the string
+* @str: parameter
+* Return: 0
+*/
+void puts_half(char *str)
+{
+	int l = 0, a, i;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+
+	a  = (l + 1) / 2;	
+
+	for (i = a; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
