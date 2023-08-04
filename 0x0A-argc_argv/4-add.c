cd5abea9e8fe496bb9c56ad78cb0d50b274050b3
OscarM03 <oscarmutwiri03@gmail.com>
2023-08-05 02:20:57 +0300

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - add numbers
+  * @argc: arg count
+  * @argv: arg
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] < '0' || argv[i][0] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
