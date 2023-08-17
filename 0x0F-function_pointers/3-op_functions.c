b185f47b9c0748e7b8dd6fa590b9ce17d3081b26
OscarM03 <oscarmutwiri03@gmail.com>
2023-08-17 15:51:45 +0300

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,31 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-/**
- * get_op_func - point to functions
- * @s: operator passed
- * Return: function of the operator passed
- */
-int (*get_op_func(char *s))(int, int)
-{
-	int i;
 
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	i = 0;
-	while (ops[i].op != NULL)
+/**
+  * op_add - add two integers
+  * @a: integer 1
+  * @b: integer 2
+  * Return: result of sum
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - substract two ints
+  * @a: int 1
+  * @b: int 2
+  * Return: result of subtraction
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - multiply two integers
+  * @a: int 1
+  * @b: int 2
+  * Return: results of miltiplication
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - division of two ints
+  * @a: int 1
+  * @b: int 2
+  * Return: div of two ints
+  */
+int op_div(int a, int b)
+{
+	if (b == 0)
 	{
-		if (s[0] == ops[i].op[0])
-		{
-			return (ops[i].f);
-		}
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-	return (NULL);
+	return (a / b);
+}
+
+/**
+  * op_mod - get the remeinder
+  * @a: int 1
+  * @b: int 2
+  * Return: rem of two ints
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
