120de90a5cc9b0240196be909462d344595c9261
OscarM03 <oscarmutwiri03@gmail.com>
2023-08-31 23:07:07 +0300

    Task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * get_endianness - checks the endianness
+  * return: void
+  */
+int get_endianness(void)
+{
+	int i = 1;
+	char *ptr;
+
+	ptr = (char *)&i;
+
+	if (*ptr == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
