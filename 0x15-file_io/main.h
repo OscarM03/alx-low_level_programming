976236e0f1b9b31f2d3be2670c213aafb169387e
OscarM03 <oscarmutwiri03@gmail.com>
2023-09-22 12:33:22 +0300

    task 3, file i/o

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,5 +17,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(char *message, char *filename, int exit_code);
+void copy_content(int source_fd, int target_fd);
 
 #endif
