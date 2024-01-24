4465be092d68aa2790c34031b2a458d247f2c634
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-25 00:01:24 +0300

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,8 @@
+#!/usr/bin/python3
 def island_perimeter(grid):
+    """This function gets the perimeter of an island"""
     perimeter = 0
     for list in grid:
         for i in list:
             perimeter = perimeter + i
     return (perimeter + 1) * 2
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
